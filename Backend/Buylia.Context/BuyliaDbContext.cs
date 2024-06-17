82d78aacf50e533c3434e1785dcda9ffc1526243
M-Moussa-De <mamo1611@hotmail.com>
2024-06-17 20:17:08 +0200

    Edite context.

--- Backend/Buylia.Context/BuyliaDbContext.cs
+++ Backend/Buylia.Context/BuyliaDbContext.cs
@@ -7,12 +7,26 @@ public class BuyliaDbContext(DbContextOptions<BuyliaDbContext> options) : DbCont
 {
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Localization> Localizations { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<ProductAttribute> ProductAttributes { get; set; }
+    public DbSet<ProductColor> ProductColors { get; set; }
+    public DbSet<ProductImage> ProductImages { get; set; }
+    public DbSet<ProductSize> ProductSizes { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Seller> Sellers { get; set; }
+    public DbSet<Tag> Tags { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
-        //builder.Entity<User>()
-        //    .HasNoKey();
+        builder.ApplyConfigurationsFromAssembly(typeof(BuyliaDbContext).Assembly);
     }
 }
